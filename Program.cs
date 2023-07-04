8fd40e374c6a20c35fdca54fadae9e7fd800684b
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-04 15:35:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Вывод_имени
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите символ: ");
+            symbol = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            PrintNameInRectangle(name, symbol);
+        }
+
+        static void PrintNameInRectangle(string name, char symbol)
+        {
+            int length = name.Length;
+
+            Console.WriteLine(new string(symbol, length + 4));
+
+            Console.WriteLine($"{symbol} {name} {symbol}");
+
+            Console.WriteLine(new string(symbol, length + 4));
+        }
+    }
+}
